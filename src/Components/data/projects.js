ddf4601f04af2e86c80971dd7b796a74e7877ad0
Abhisek753 <properachievement@gmail.com>
2023-07-04 14:05:13 +0530

    todo project

--- src/Components/data/projects.js
+++ src/Components/data/projects.js
@@ -15,14 +15,25 @@ export const ProjectData=[
         title:"KOOVS CLONE (Kapdelo)",
         about:"Kapdelo is a Ecomerse related website which helps you to find the different kinds of the products. Kapdelo is part of THG Holdings plc, a globally renowned end-to-end tech platform specializing in taking brands direct to consumers across the globe.",
         tags:["JAVASCRIPT","MongoDb","REACT","REDUX","CYPRESS","CHAKRA UI"],
-        demo:"https://frontend-mohit-6164.vercel.app/",
+        demo:"https://koovs-mu.vercel.app/",
         github:"https://github.com/sukanta255/ordinary-summer-5089",
         image:require("../../portfolio_img/Koovs.com.png"),
         
         
     },
     {
-        id:2,
+        id:3,
+        title:"Todo App",
+        about:"A todo app is a software application that allows users to create and manage tasks or items on a to-do list.",
+        tags:["JAVASCRIPT","REACT","HTML","CSS","json-server"],
+        demo:"https://crudapp-nine.vercel.app",
+        github:"https://gitlab.com/Abhisek753/crud_operation",
+        image:require("../../portfolio_img/React_Todo.png"),
+        
+        
+    },
+    {
+        id:4,
         title:"HOTSTAR CLONE",
         about:" Disney+ Hotstar is the dedicated streaming home for global and Malaysian hits. It brings together blockbuster Hollywood movies and award-winning content from Disney, Marvel, Star Wars, Pixar, National Geographic, and many more, which also includes exclusive premieres and blockbusters from leading Malaysian studios. Disney+ Hotstar is an online video streaming platform. An individual project built in 5 days.",
         tags:["HTML","CSS","JAVASCRIPT"],
@@ -33,7 +44,7 @@ export const ProjectData=[
         
     },
     {
-        id:4,
+        id:5,
         title:"MENTIMETER CLONE",
         about:" Mentimeter enables you to: Engage with students using live polls, word clouds, quizzes, multiple-choice questions and more. Track learning and understanding by asking questions and downloading results. Communicate and interact with your students.",
         tags:["HTML","CSS","JAVASCRIPT"],
@@ -44,7 +55,7 @@ export const ProjectData=[
         
     },
     {
-        id:5,
+        id:6,
         title:"Machine & Tools",
         about:" We are one-of-its-kind online platform, featuring an exclusive range of utility products for all industrial purposes. Industrybuying began its operations during 2013, with an objective to inculcate and introduce the concept of industrial supply chains in India. Currently our website maintains a fully catalogued collection of over 3.8 Lac SKUs, from brands across global geographies.",
         tags:["HTML","CSS","JAVASCRIPT","REACT","REDUX","CHAKRA UI","FIREBASE"],
